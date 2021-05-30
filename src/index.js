1203fef80852905c870a84cfcc13163333ea9c8d
VinoliaBarreto <81826927+VinoliaBarreto@users.noreply.github.com>
2021-05-30 14:04:42 +0100

    Created new function displayForecast and add an id in inde.xhtml

--- src/index.js
+++ src/index.js
@@ -38,10 +38,46 @@ function insertCity(event) {
 let form = document.querySelector("form");
 form.addEventListener("submit", insertCity);
 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElem = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function getForecast(coordinates) {
   let apiKey = "fceab41571af72ba31162dfdc25877b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
-  console.log(apiUrl);
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showEvent(response) {

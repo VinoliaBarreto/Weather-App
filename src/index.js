bde32c56d865715e156807a8a9fde1dd0e34027f
VinoliaBarreto <81826927+VinoliaBarreto@users.noreply.github.com>
2021-05-30 13:17:22 +0100

    Console apiUrl

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ form.addEventListener("submit", insertCity);
 function getForecast(coordinates) {
   let apiKey = "fceab41571af72ba31162dfdc25877b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
 }
 
 function showEvent(response) {
@@ -67,9 +67,4 @@ function showEvent(response) {
   win.innerHTML = `Wind: ${windy} Km/h`;
 
   getForecast(response.data.coord);
-}
-
-function diplayForecast(response) {
-  console.log(response);
-  //let forecast = response.data.daily;
 }
